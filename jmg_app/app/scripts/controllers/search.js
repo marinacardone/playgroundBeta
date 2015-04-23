112fd9a462487686566a2bd861511af450b4f692
Marina <marinacardone@gmail.com>
2015-04-22 23:26:58 -0300

    Search page updates: search service implemented

--- jmg_app/app/scripts/controllers/search.js
+++ jmg_app/app/scripts/controllers/search.js
@@ -8,7 +8,22 @@
  * Controller of the wisableApp
  */
 angular.module('wisableApp')
-  .controller('SearchController', ['$scope', '$http', '$location' , 'Session' , 'Interests', function ($scope, $http, $location, Session, Interests) {
+  .controller('SearchController', ['$scope', '$http', '$location' , 'Session' , 'articlesServerProvider', function ($scope, $http, $location, Session, articlesServerProvider) {
     var userId = Session.userId;
+
+    //http://python-recommender.herokuapp.com/articles/search/giraldezblack@gmail.com/boca%20juniors/
+
+    var baseURL = articlesServerProvider+'search/';
+
+    $scope.doSearch = function(searchQuery){
+        $http.get(baseURL + userId + '/' +encodeURIComponent(searchQuery))
+        .then(function(response) {
+            $scope.articles = response.data;
+        }, function(errResponse) {
+          console.error('Error while fetching articles. ' + errResponse);
+      });
+    }
+
+      
 
   }]);
